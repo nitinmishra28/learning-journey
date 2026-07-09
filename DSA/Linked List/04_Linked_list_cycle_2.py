b567c489d83170e27894a2a7c1b96c5a430dd846
Nitin <nitin.mishra@bittwobyte.com>
2026-07-09 20:18:00 +0530

    Add hash set and Floyd's cycle detection solutions for LeetCode 142: Linked List Cycle II

---
+++ DSA/Linked List/04_Linked_list_cycle_2.py
@@ -0,0 +1,62 @@
+# 142. Linked List Cycle II
+
+# Given the head of a linked list, return the node where the cycle begins.
+# If there is no cycle, return None.
+
+
+# ------------------------------------------------------------
+# Solution 1 - Hash Set
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# ------------------------------------------------------------
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        visited = set()
+        curr = head
+
+        while curr:
+            if curr in visited:
+                return curr
+
+            visited.add(curr)
+            curr = curr.next
+
+        return None
+
+
+# ------------------------------------------------------------
+# Solution 2 - Floyd's Cycle Detection Algorithm (Optimal)
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# ------------------------------------------------------------
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+
+        # Detect whether a cycle exists
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                break
+        else:
+            return None
+
+        # Find the starting node of the cycle
+        slow = head
+
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+
+        return slow
